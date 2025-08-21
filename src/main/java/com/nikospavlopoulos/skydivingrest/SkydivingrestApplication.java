bfdba3e2d9c7dba1c4a77a9978035ee9837fa7db
Nikos Pavlopoulos <npavlopoulos@gmail.com>
2025-08-21 14:18:03 +0300

    Initial commit

---
+++ src/main/java/com/nikospavlopoulos/skydivingrest/SkydivingrestApplication.java
@@ -0,0 +1,13 @@
+package com.nikospavlopoulos.skydivingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkydivingrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkydivingrestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikospavlopoulos/skydivingrest/SkydivingrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikospavlopoulos.skydivingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkydivingrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
